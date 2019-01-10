980033b8c31c8f3ecc643df225f729024ce2aa51
Patrick Quan <patrickquan@Patricks-MBP.hitronhub.home>
2019-01-09 17:26:40 -0700

    Added keys

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mLabID: "mongodb://admin:capturepass88@ds115124.mlab.com:15124/capturephotographycanada",
+  passportSecret: "Theo and Amelia are the best!",
+  cloudinaryName: "dzpvgtcdp",
+  cloudinaryKey: "694265316154491",
+  cloudinarySecret: "EQfA1I-MAYQmfl2WzJHeAijgxo4",
+  nodemailerUser: "mySMTP.17@gmail.com",
+  nodemailerPass: "kuqVuz-tempab-4hurhy"
+}
